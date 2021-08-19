80429635c2029fd82e8f8ad80d5c4610c7c998bb
Gary Pavlis <pavlis@indiana.edu>
2021-08-19 05:58:17 -0400

    Beautified with bcpp

--- cxx/src/pwmigcore/pwmig/mdutils.cc
+++ cxx/src/pwmigcore/pwmig/mdutils.cc
@@ -12,35 +12,37 @@ using namespace SEISPP;
 
 SlownessVector get_stack_slowness(ThreeComponentEnsemble& ensemble)
 {
-	SlownessVector slow;
+  SlownessVector slow;
 
-	// We extract these from the global metadata area for the ensemle.
-	// conceptually correct, but requires more careful implementation and assumes
-	// the data are there.
-	try {
-		slow.ux = ensemble.get_double("plane_wave_ux");
-		slow.uy = ensemble.get_double("plane_wave_uy");
-	} catch (MetadataError& mde)
-	{
-		throw(mde);
-	}
-	return(slow);
+  // We extract these from the global metadata area for the ensemle.
+  // conceptually correct, but requires more careful implementation and assumes
+  // the data are there.
+  try
+  {
+    slow.ux = ensemble.get_double("plane_wave_ux");
+    slow.uy = ensemble.get_double("plane_wave_uy");
+  } catch (MetadataError& mde)
+  {
+    throw(mde);
+  }
+  return(slow);
 }
+
 
 /*
 Hypocenter get_event_hypocenter(ThreeComponentEnsemble& ensemble)
 {
-	Hypocenter h;
+  Hypocenter h;
 
-	try {
-		h.lat = ensemble.get_double("origin.lat");
-		h.lon = ensemble.get_double("origin.lon");
-		h.z = ensemble.get_double("origin.depth");
-		h.time = ensemble.get_double("origin.time");
-	} catch (MetadataError& mde)
-	{
-		throw mde;
-	}
-	return(h);
+  try {
+    h.lat = ensemble.get_double("origin.lat");
+    h.lon = ensemble.get_double("origin.lon");
+    h.z = ensemble.get_double("origin.depth");
+    h.time = ensemble.get_double("origin.time");
+  } catch (MetadataError& mde)
+{
+throw mde;
+}
+return(h);
 }
 */
