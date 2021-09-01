50e6a71a3087293aaa3fe6aa91d907cf27e8127f
Gary Pavlis <pavlis@indiana.edu>
2021-09-01 08:02:54 -0400

    New function implementing innermost loop of pwmig for use with python driver

--- cxx/src/pwmigcore/pwmig/migrate_one_seismogram.cc
+++ cxx/src/pwmigcore/pwmig/migrate_one_seismogram.cc
@@ -1,42 +1,73 @@
-bool weight_functions_set=false;
-// Warning:  n3 must not change in the loop below
-int n3=raygrid.n3;
-vector<double> domega_ij(n3),dweight_ij(n3);
-for(is=0;is<pwdata->member.size();++is)
+
+
+PWMIGmigrated_seismogram migrate_one_seismogram(Seismogram& pwddata,
+      const GCLgrid3d& raygrid
+        const GCLscalarfield3d& TPgrid,
+         const bool ApplyElevationStatics,
+          const bool use_grt_weights
+            const bool stack_only)
 {
+  /* This gets a bit messy to mesh with special class we return that isn't
+  quite a Seismogram. */
+  if(pwddata.dead())
+  {
+    PWMIGmigrated_seismogram deadguy;
+    deadguy.elog=pwdata.elog;
+    if(pwdata.is_defined("ix1"))
+    {
+      deadguy.ix1=pwdata.get_int("ix1");
+    }
+    if(pwdata.is_defied("ix2"))
+    {
+      deadguy.ix2=pwdata.get_int("ix2");
+    }
+    /* Make sure he is dead*/
+    deadguy.live=false;
+    return deadguy;
+  }
+  bool weight_functions_set=false;
   // convenient shorthand variables.  ns is data length
   // while n3 is the ray path onto which data are mapped
-  int ns=pwdata->member[is].ns;
+  int ns=pwdata.ns;
+  int n3=raygrid.n3;
   double zmaxray,tmaxray;
   vector<double> Stime(n3), SPtime(n3);
   double t0;
-
-  dt=pwdata->member[is].dt;
-  // first decide in which grid cell to place this seismogram
-  i = pwdata->member[is].get_int("ix1");
-  j = pwdata->member[is].get_int("ix2");
+  dt=pwdata.dt;
+  /* first decide in which grid cell to place this seismogram
+  WARNING:  don't use i, and j as loop indices as is common in the rest of
+  this function */
+  int i,j,gridid;
+  i = pwdata.get_int("ix1");
+  j = pwdata.get_int("ix2");
+  gridid=pwdata.get_int("gridid");
   if(ApplyElevationStatics)
   {
-    /* We assume elevation has been posted as elev.  This is done
-    by PwmigFileHandle methods on loading, but if data handling method
-    changes this needs to be watched.  Be aware this elevation is produced
-    in pwstack as a weighted average of elevations of summed traces */
-    double pselev=pwdata->member[is].get_double("elev");
-    t0=pwdata->member[is].t0;
-    ApplyGeometricStatic(dynamic_cast<BasicTimeSeries *>(&(pwdata->member[is])),
+    double pselev;
+    if(pwdata.is_defined())
+      pselev=pwdata.get_double("elev");
+    }
+    else
+    {
+      /* This condition should, perhaps, generate a warning message in elog
+      if I decide to add elog to the GCLfield objects. */
+      pselev=0.0;
+    }
+    ApplyGeometricStatic(dynamic_cast<BasicTimeSeries *>(&(pwdata)),
       static_velocity,pselev);
-    if(SEISPP_verbose)
-      cout << "Applying static time shift = "
-        << pwdata->member[is].t0 - t0<<endl;
   }
-  t0=pwdata->member[is].t0;
+  t0=pwdata.t0;
   if( (i<0) || (i>raygrid.n1) || (j<0) || (j>raygrid.n2) )
   {
-    cerr << "grid index of data = ("
+    /* We have this condition throw an exception because it only happens if
+    something is corrupted in the data or there is a fatal mismatch of parameters.
+    The planned parameter checker should test for this condition*/
+    stringstream ss;
+    ss << "migrate_one_seismogram:  grid index of data = ("
       << i << ","<< j
       << "is outside range of grid." <<endl
       << "Check grid definitions in database and pf"<<endl;
-    exit(-1);
+    throw MsPASSError(ss.str(),ErrorSeverity::Fatal);
   }
   // We need a reference ray for the incident wavefield.
   // This ray is chopped up and transformed to GCLgrid coordinates
@@ -69,11 +100,7 @@ for(is=0;is<pwdata->member.size();++is)
   // the S ray path (pathptr) is oriented downward.
   // Only need to compute P time to surface once for
   // each ray so we compute it before the loop below
-  /* old form
-                             double Tpr=compute_receiver_Ptime(raygrid,i,j,
-                                      *TPptr,hypo);
-                                      */
-  double Tpr=TPptr->val[i+border_pad][j+border_pad][TPptr->n3-1];
+  double Tpr=TPgrid.val[i+border_pad][j+border_pad][TPgrid.n3-1];
   double tlag,Tpx,tdelta;
   bool needs_padding;
   int padmark=raygrid.n3-1;
@@ -82,22 +109,22 @@ for(is=0;is<pwdata->member.size();++is)
    * Common for all in next loop and needed for travel time calculation.
    * Note the ugly border pad construct necessary to account for padding
    * efficiently - avoids a lookup */
-  Geographic_point rTP_gp=fetch_TP_point_at_base(*TPptr,i+border_pad,j+border_pad);
-  /* It is a good idea to initialize these before each raty path */
-  TPptr->reset_index();
+  Geographic_point rTP_gp=fetch_TP_point_at_base(TPgrid,i+border_pad,j+border_pad);
+  TPgrid.reset_index();
   SPtime.clear();
   tcompute_problem=false;
   needs_padding=false;
+  SlownessVector u0=svm0(i,j);
+  PWMIGmigrated_seismogram result(i,j,n3);
   for(k=0,kk=raygrid.n3-1;k<raygrid.n3;++k,--kk)
   {
     vector<double>nu;
     double vp;
-    SlownessVector u0=svm0(i,j);
     /* This is needed below to compute p*delta term */
     Geographic_point x_gp,rxTP_gp0;
     x_gp=raygrid.geo_coordinates(i,j,kk);
 
-    nu = compute_unit_normal_P(*TPptr,raygrid.x1[i][j][kk],
+    nu = compute_unit_normal_P(TPgrid,raygrid.x1[i][j][kk],
       raygrid.x2[i][j][kk], raygrid.x3[i][j][kk]);
     for(l=0;l<3;++l) nup(l,kk)=nu[l];
     // zP and gradTp are computed in reverse order
@@ -107,12 +134,12 @@ for(is=0;is<pwdata->member.size();++is)
     /* This section used to be a procedure.  Inlined for
     speed during a cleanup June 2012 */
     int error_lookup;
-    error_lookup=TPptr->lookup(raygrid.x1[i][j][kk],
+    error_lookup=TPgrid.lookup(raygrid.x1[i][j][kk],
       raygrid.x2[i][j][kk],raygrid.x3[i][j][kk]);
     switch(error_lookup)
     {
       case 0:
-        Tpx=TPptr->interpolate(raygrid.x1[i][j][kk],
+        Tpx=TPgrid.interpolate(raygrid.x1[i][j][kk],
           raygrid.x2[i][j][kk],raygrid.x3[i][j][kk]);
         break;
       case 1:
@@ -135,7 +162,7 @@ for(is=0;is<pwdata->member.size();++is)
      * no antelope version pwmig2.0 */
     try
     {
-      rxTP_gp0=get_gp_base_TPx(*TPptr,x_gp);
+      rxTP_gp0=get_gp_base_TPx(TPgrid,x_gp);
     }catch(SeisppError& serr)
     {
       serr.log_error();
@@ -147,18 +174,14 @@ for(is=0;is<pwdata->member.size();++is)
      * here to correct for p*delta */
     tdelta=compute_delta_p_term(rxTP_gp0,rTP_gp,u0);
     tlag=Tpx+Stime[k]+tdelta-Tpr;
-    //DEBUG
-    /*cout << "k="<<k<<"Tpx="<<Tpx<<" Stime[k]="<<Stime[k]
-        << " tdelta="<<tdelta<<" Tpr="<<Tpr<<" = tlag of "<<tlag<<endl;
-        */
-    //cout << "tdelta="<<tdelta <<" tlag="<<tlag<<endl;
     SPtime.push_back(tlag);
   }
 
   // skip this ray if there were travel time computation problems
   if(tcompute_problem)
   {
-    cerr << "Warning:  slowness gridid "<< gridid
+    stringstream ss;
+    ss << "Warning:  slowness gridid "<< gridid
       << ", grid position index ("
       << i << ","
       << j << ").  GCLscalarfield3d lookup failed at ray index="
@@ -169,19 +192,21 @@ for(is=0;is<pwdata->member.size();++is)
       << "Data from failed ray index downward will be zeroed"<<endl
       << "This may leave ugly holes in the output image."
       <<endl;
-
+    result.elog.log_error(ss.str(),ErrorSeverity::Complaint);
   }
   if (needs_padding)
   {
     if(padmark==0)
     {
-      cerr << "Warming:   gridid "<< gridid
+      stringstream ss;
+      ss << "Warning:   gridid "<< gridid
         << ", grid position index ("
         << i << ","
         << j << ")." <<endl
         << "P time interpolation failed at earth's surface.  "<<endl
         << "This should not happen and may cause holes in the output."<<endl
         << "This trace will be projected as zeros"<<endl;
+      result.elog.log_error(ss.str(),ErrorSeverity::Complaint);
     }
     else
     {
@@ -195,82 +220,25 @@ for(is=0;is<pwdata->member.size();++is)
   }
   // We now interpolate the data with tlag values to map
   // the data from time to an absolute location in space
-  // Note carefully SPtime and work sense is inverted from
+  // Note carefully SPtime and result.migrated_data sense are inverted from
   // the raygrid.  i.e. they are oriented for a ray from the surface
   // to the bottom.  Below we have to reverse this
-  dmatrix work(3,raygrid.n3);
-  linear_vector_regular_to_irregular(t0,dt,pwdata->member[is].u,
-    &(SPtime[0]),work);
+
+  linear_vector_regular_to_irregular(t0,dt,pwdata.u,
+    &(SPtime[0]),result.migrated_data);
   /* Cosine aper all data that was padded and zero the extension */
   if(needs_padding)
-    cosine_taper_highend(work,padmark,taper_length);
-  dmatrix raycoh(4,coh3cens->member[is].ns);
-  int ncohcopy=coh3cens->member[i].ns;
-  /* excessively paranoid, but murphy's law */
-  if(cohens->member[is].ns != ncohcopy)
-  {
-    cerr << "Warning:  inconsistent coherence data size"<<endl
-      << "Three-C data size="<<ncohcopy
-      << " while all component sum size="
-      << cohens->member[is].ns<<endl;
-    ncohcopy=min(cohens->member[is].ns,ncohcopy);
-    cerr << "Set to minimum="<<ncohcopy<<endl;
-  }
-  /* This component coherence into first 3 rows and sum in 4 */
-  dcopy(ncohcopy,coh3cens->member[is].u.get_address(0,0),3,
-    raycoh.get_address(0,0),4);
-  dcopy(ncohcopy,coh3cens->member[is].u.get_address(1,0),3,
-    raycoh.get_address(1,0),4);
-  dcopy(ncohcopy,coh3cens->member[is].u.get_address(2,0),3,
-    raycoh.get_address(2,0),4);
-  dcopy(ncohcopy,&(cohens->member[is].s[0]),1,
-    raycoh.get_address(3,0),4);
-  int nout=SPtime.size();
-  dmatrix raycohout(4,nout);
-  /* we assume t0 and dt are consistent or scalar and vector
-  coherence data */
-  linear_vector_regular_to_irregular(cohens->member[is].t0,
-    cohens->member[is].dt, raycoh,&(SPtime[0]),raycohout);
-  /* This nontrivial effort may not be required, but best to
-  have it available until proven to be a big issue. */
-  raycohout=running_average(raycohout,cohsl);
-  /* Now copy these results to the coherence raygrid.
-  Note carefully the reverse order going from raycohout order
-  to rayrid order and decimation by cohdecfac */
-  for(k=0,kk=pwcohgrid.n3-1;(k<nout) && (kk>=0);
-    k+=cohdecfac,--kk)
-  {
-    for(l=0;l<4;++l)
-      pwcohgrid.val[i][j][kk][l]=raycohout(l,k);
-  }
+    cosine_taper_highend(result.migrated_data,padmark,taper_length);
 
   //
   // Compute the transformation vector for each
-  // ray point in space.  This comes in two flavors.
-  // A fast, inexact version and a exact slow version
+  // ray point in space.
   //
   dmatrix *pathptr=extract_gridline(raygrid,i,j,raygrid.n3-1,3,true);
-  if(use_depth_variable_transformation)
-  {
-    // the slow, exact method
-    troptr=new Ray_Transformation_Operator(
-      parent,
-      *pathptr,
-      ustack.azimuth(),
-      nup);
-  }
-  else
-  {
-    // the fast approximation
-    troptr=new Ray_Transformation_Operator(
-      parent,
-      *pathptr,ustack.azimuth());
-  }
-  Ray_Transformation_Operator& trans_operator=*troptr;
-  work=trans_operator.apply(work);
+  Ray_Transformation_Operator trans_operator(parent,*pathptr,ustack.azimuth(),nup);
+  result.migrated_data=trans_operator.apply(result.migrated_data);
   // done with these now
   delete pathptr;
-  delete troptr;
 
   // This computes domega for a ray path in constant dz mode
   // We would have to interpolate anyway to mesh with
@@ -283,43 +251,27 @@ for(is=0;is<pwdata->member.size();++is)
     do it right some day */
     if(stack_only)
     {
-      domega_ij.clear();
-      for(k=0;k<n3;++k) domega_ij.push_back(1.0);
+      result.domega.clear();
+      for(k=0;k<n3;++k) result.domega.push_back(1.0);
     }
     else
     {
       /* This is the normal block for computing solid angles */
-      domega_ij=compute_domega_for_path(ustack,dux,duy,
+      result.domega=compute_domega_for_path(ustack,dux,duy,
         Vs1d, zmaxray,dz,
         raygrid, i, j, gradTp,zP);
     }
     if(use_grt_weights && (!stack_only) )
-      dweight_ij=compute_weight_for_path(gradTp,gradTs);
+      result.dweight=compute_weight_for_path(gradTp,gradTs);
     else
-      for(k=0;k<n3;++k)dweight_ij[k]=1.0;
+      for(k=0;k<n3;++k)result.dweight[k]=1.0;
     if(smooth_wt && (!stack_only))
     {
-      domega_ij=running_average(domega_ij,nwtsmooth);
+      result.domega=running_average(result.domega,nwtsmooth);
       // Unnecessary when not using grt weighting
       if(use_grt_weights)
-        dweight_ij=running_average(dweight_ij,nwtsmooth);
+        result.dweight=running_average(result.dweight,nwtsmooth);
     }
     weight_functions_set=true;
   }
-
-  //
-  // copy transformed data to vector field
-  // copy weights and domega at same time
-  //
-  for(k=0,kk=raygrid.n3-1;k<raygrid.n3;++k,--kk)
-  {
-    for(l=0;l<3;++l)
-    {
-      pwdgrid.val[i][j][k][l]=work(l,kk)
-        *dweight_ij[kk]*domega_ij[kk];
-    }
-    pwdgrid.val[i][j][k][3]=domega_ij[kk];
-    pwdgrid.val[i][j][k][4]=dweight_ij[kk];
-  }
-  }
-  delete pwdata;
+}
