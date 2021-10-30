3fdb0cd871bedab0bdb74e389be210373dddf033
Gary Pavlis <pavlis@indiana.edu>
2021-10-30 08:22:20 -0400

    Several changes for initial tests - incomplete but want to save this version

--- python/pwmigpy/pwmig/pwstack.py
+++ python/pwmigpy/pwmig/pwstack.py
@@ -10,18 +10,23 @@ is highly parallelizable and preferable to be  run parallel.
 import dask
 import math
 
-from mspasspy.ccore.seismic import SeismogramEnsemble,TopMute
+
+from mspasspy.ccore.seismic import SeismogramEnsemble
+from mspasspy.algorithms.window import TopMute
 from mspasspy.ccore.utility import MsPASSError
-from pwmig.ccore.pwmigcore import (RectangularSlownessGrid,
+from pwmigpy.ccore.pwmigcore import (RectangularSlownessGrid,
                                    DepthDependentAperture,
                                    pwstack_ensemble)
-from pwmig.ccore.gclgrid import GCLscalarfield,GCLdbread
+from pwmigpy.ccore.gclgrid import GCLscalarfield
+from pwmigpy.db.database import GCLdbread
 
 def TopMuteFromPf(pf,tag):
     """
     The C++ class TopMute does not have an AntelopePf driven constructor.
     This function is a front end to work around that in python.  
     Could have been done in C++ but a trivial constuct either way.
+    The mutetype is required for mspass implementation.  We default it 
+    here to cosine for backward compability with old pwstack pf files.
     
     :param pf:  AntelopePf that is assumed to have a branch with label tag
     :param tag: string defining branch name to extract
@@ -30,9 +35,21 @@ def TopMuteFromPf(pf,tag):
 
     """
     pfbranch=pf.get_branch(tag)
-    mutetype=pfbranch.get_string('mute_type')
-    t0=pfbranch.get_double('t0')
-    t1=pfbranch.get_double('t1')
+    if pfbranch.is_defined('mute_type'):
+        mutetype=pfbranch.get_string('mute_type')
+    else:
+        mutetype = 'cosine'
+    # the old pwstack names for these parameters were very confusing
+    # documentation should say to use t0 and t1 but accept the old 
+    # names for backward compatibility with old pf files.
+    if pfbranch.is_defined('t0'):
+        t0=pfbranch.get_double('t0')
+    else:
+        t0=pfbranch.get_double('zero_end_time')
+    if pfbranch.is_defined('t1'):
+        t1=pfbranch.get_double('t1')
+    else:
+        t1=pfbranch.get_double('end_time')
     return TopMute(t0,t1,mutetype)
         
 class pwstack_control:
@@ -56,13 +73,14 @@ class pwstack_control:
             fvp=pf.get_double("fresnel_vp")
             fcm=pf.get_double("fresnel_cutoff_multiplier")
             fperiod=pf.get_double("fresnel_period")
-            fdt=pf.get_double("fresnel_lag_time_sample_interval")
-            fnt=pf.get_int("fresnel_number_lag_samples")
+            fdt=pf.get_double("fresnel_lag_time_sampling_interval")
+            fnt=pf.get_long("fresnel_number_lag_samples")
             self.aperture=DepthDependentAperture(fvs,fvp,fperiod,fdt,fnt,fcm,True)
         else:
             self.aperture=DepthDependentAperture(pf,"depth_dependent_aperture")
         self.aperture_taper_length=pf.get_double("aperture_taper_length")
         self.centroid_cutoff=pf.get_double("centroid_cutoff")
+        self.fold_cutoff = pf.get_long("stack_count_cutoff")
         self.save_history=save_history 
         self.algid=instance
         # These parameters define thing things outside pwstack_ensemble 
@@ -83,7 +101,7 @@ class pwstack_control:
         self.stagrid=GCLdbread(db,doc)
         self.fold=GCLscalarfield(self.stagrid)
 
-def site_query(db,lat,lon,ix1,ix2,cutoff):
+def site_query(db,lat,lon,ix1,ix2,cutoff,units='km'):
     """
     This function is called in in parallel to do a query to the site 
     collection for stations with a center at lat, lon within 
@@ -94,40 +112,52 @@ def site_query(db,lat,lon,ix1,ix2,cutoff):
     :param lon: longitude (in degrees) of center point
     :param cutoff:  distance in degrees of circlular region centered on 
        lat,lon to select. 
+    :param units:  string defining units of cutoff argument.   Note 
+      lon and lat are always assumed to be degrees.  Acceptable values 
+      are 'km', 'degrees', or 'radians'.  Default is km
        
     :return:  python list of ObjectIds of site documents linked to 
       stations within the search area (note this includes all times so 
       a station location can appear more than once. Assume when matched 
-      against wf collection this will become one-to-one.
+      against wf collection this will become one-to-one.  Returns and 
+      empty list if the query returns no site documents.
 
     """
-    # cutoff is assumed in degrees but mongo wants that in radians
-    rcutoff=math.rad(cutoff)
-    query={'coordinates' :
-           {'$nearSphere' : 
-            { '$geometry' : 
-                { 
-                    'type' : 'Point',
-                    'coordinates' : [ lon , lat ]
-                },
-                '$maxDistance' : rcutoff
-             }
+    if units=='radians':
+        rcutoff = cutoff  # query we use requires distance in radians
+    elif units=='km':
+        rcutoff = cutoff/6371.0  # might want to use a more precise earth radius in km here
+    elif units=='degrees':
+        rcutoff = math.radians(cutoff)
+
+    #query = { "coordinates" : SON( [ ("$nearSphere" , [ lon , lat ]),
+    #             ("$maxDistance" , rcutoff) ] )
+    #       }
+    query = { "coordinates" : {
+          "$geoWithin" : {
+              "$centerSphere" : [ [lon, lat],rcutoff]
            }
+         }
         }
-    cursor=db.site.find(query)
+    n=db.site.count_documents(query)
+    # debug
+    #print(lon,lat,ix1,ix2,n)
+    
     idlist=list()
-    for doc in cursor:
-        id=doc['_id']
-        idlist.append(id)
+    if n>0:
+        cursor=db.site.find(query)
+        for doc in cursor:
+            thisid=doc['_id']
+            idlist.append(thisid)
     result=dict()
     result['idlist']=idlist
     result['lat']=lat
     result['lon']=lon
     result['ix1']=ix1
     result['ix2']=ix2
-
     return result
-def build_wfquery(sid,ridlist):
+
+def build_wfquery(sid,rids):
     """
     This function is more or less a reformatter.  It expands each 
     source_id by a set of queries for a pseudostation grid 
@@ -141,7 +171,7 @@ def build_wfquery(sid,ridlist):
     :param sid:  expected to contain a single ObjectID the resolves 
       with the source collection and defines the seismic source 
       data to be processed (pwmig is prestack in that sense)
-    :param ridlist: is a nested data structure.  It is expected to 
+    :param rids: is a nested data structure.  It is expected to 
       contain a list of dict structures.  As a minimum each dict for 
       this function is expected to contain data with the key 'idlist'.
       The value associated with idlist is assumed to be a list of 
@@ -149,7 +179,8 @@ def build_wfquery(sid,ridlist):
       station data that can be stacked to define each pseudostation grid 
       position.  The function passes along the attributes that define 
       the pseudostation point - lat, lon, and two integer indicec 
-      (ix1, and ix2)
+      (ix1, and ix2)   The return is mostly the contents of this plus
+      the source data
     :return: a list of dict data expanded from sid with one entry 
       per pseudostation defined in ridlist.  The contents of each dict 
       are copies of lat, lon, ix1, and ix2 plus  'query' attribute that 
@@ -158,35 +189,43 @@ def build_wfquery(sid,ridlist):
       if the ridlist received is empty.   That is common with a sparse 
       and/or irregular station geometry.
     """
-    if len(ridlist)==0:
-        return list()
-    else:
-        allqdata=list()
-        for rid in ridlist:
-            # Safer to make a copy of this for a tiny cost
-            allq=dict(rid)
-            q=dict()
-            q['source_id'] = { '$eq' : sid }
-            q['site_id'] = { '$in' : rid['idlist']}
-            allq['query']=q
-            allq.popitem['idlist']
-            allqdata.append(allq)
-        return allqdata
+    allqdata = rids.copy()
+    q=dict()
+    q['source_id'] = { '$eq' : sid }
+    idlist = rids['idlist']
+    q['site_id'] = { '$in' : rids['idlist']}
+    allqdata['query']=q
+    # We use this field for efficiency.   It is known from experience 
+    # with pwmig that a stack cutoff is required because low fold 
+    # pseudostation bins produce bad results.  Hence, we post the number of 
+    # stations in each ensemble and drop anything when this count is too 
+    # small
+    allqdata['fold'] = len(idlist)
+    return allqdata
 
-def read_ensembles(db,querydata):
+def read_ensembles(db,querydata,control):
     """
     Constructs a query from dict created by build_wfquery, runs it 
     on the wf_Seismogram collection of db, and then calls read_ensemble_data 
     on the cursor returned by MongoDB.  It the sets the ensemble 
     metadata for lat, lon, ix1, and ix2 before returning the ensembles
+    
+    We need to pass the control class to access the fold_cutoff attribute.
     """
-    query=querydata['query']
-    n=db.wf_Seismogram.count_documents(query)
-    if n==0:
+    # don't even issue a query if the fold is too low
+    fold=querydata['fold']
+    if fold<=control.fold_cutoff:
         d=SeismogramEnsemble()
     else:
-        cursor=db.wf_Seismogram.find(query)
-        d=db.read_ensemble_data(cursor,collection='wf_Seismogram')
+        query=querydata['query']
+        n=db.wf_Seismogram.count_documents(query)
+        if n==0:
+            # This shouldn't ever really be executed unless fold_cutoff 
+            # is 0 or the query is botched
+            d=SeismogramEnsemble()
+        else:
+            cursor=db.wf_Seismogram.find(query)
+            d=db.read_ensemble_data(cursor,collection='wf_Seismogram')
     # We always load these into ensemble's metadata even if the ensemble 
     # has no data.  Null data are otherwise ambiguous.   With live data 
     # these are required for running the C++ code pwstack_ensemble
@@ -199,8 +238,8 @@ def read_ensembles(db,querydata):
             
 def pwstack(db,pf,source_query=None,
         slowness_grid_tag='RectangularSlownessGrid',
-            data_mute_tag='data_top_mute',
-                 stack_mute_tag='stack_top_mute',
+            data_mute_tag='Data_Top_Mute',
+                 stack_mute_tag='Stack_Top_Mute',
                      save_history=False,instance='undefined'):
     # the control structure pretty much encapsulates the args for 
     # this driver function
@@ -216,13 +255,13 @@ def pwstack(db,pf,source_query=None,
     # source side stack was previously completed. 
     source_id_list=list()
     for doc in base_cursor:
-        id=doc['id']
+        id=doc['_id']
         source_id_list.append(id)
     #TODO  Need to add this method to api
     cutoff=control.aperture.maximum_aperture()
     staids=list()
-    for i in range(control.stagrid.nx1):
-        for j in range(control.stagrid.nx2):
+    for i in range(control.stagrid.n1):
+        for j in range(control.stagrid.n2):
 
             # We use this instead of the lat and lon methods for 
             # a minor efficiency difference.  lat and lon have to call 
@@ -230,27 +269,38 @@ def pwstack(db,pf,source_query=None,
             gc=control.stagrid.geo_coordinates(i,j)
             lat=math.degrees(gc.lat)
             lon=math.degrees(gc.lon)
-            # note this function returns a dict.  staids is then a 
-            # list of dict 
-            ids=dask.delayed(site_query)(db,lat,lon,i,j,cutoff)
+            # I could not make this work.   I don't think there is a 
+            # huge overhead in creating this geometry object defining 
+            # how to find which stations are related to each pseudostation
+            # point.
+            #ids=dask.delayed(site_query)(db,lat,lon,i,j,cutoff)
+            ids=site_query(db,lat,lon,i,j,cutoff)
             staids.append(ids)
     # I think we need to do for the steps below to work - we need data in 
     # staids
-    staids.compute()
+    #staids.compute()
     # Now we create a dask bag with query strings for all source_ids and 
-    # all staids.  We do that in parallel and put the result in bag 
-    # that we will use for downstream processing of this whole workflow
-    # i.e. everything from here on is a parallel workflow
-    mybag=dask.bag()
+    # all staids.  For now we use a large memory model and build the 
+    # list of build_query returns and then construct a bag from that. 
+    allqueries=list()
     for sid in source_id_list:
         for rids in staids:
             # build_wfquery returns a dict with lat, lon, 
-            # i, j, and a query string
-            q=dask.delayed(build_wfquery)(sid,rids)
-            mybag.append(q)
+            # i, j, and a query string  That is a simple 
+            # construct so don't think it will be a bottleneck.
+            # May have been better done with a dataframe
+            #q=dask.delayed(build_wfquery)(sid,rids)
+            q=build_wfquery(sid,rids)
+            # debug
+            print(q['ix1'],q['ix2'],q['fold'])
+            allqueries.append(q)
+    mybag=dask.bag.from_sequence(allqueries)
+    # These can now be deleted to save memory 
+    del source_id_list
+    del staids
     # parallel reader - result is a bag of ensembles created from 
     # queries held in query
-    mybag.map(lambda q : read_ensembles(q))
+    mybag.map(lambda q : read_ensembles(db,q,control))
     # Now run pwstack_ensemble - it has a long arg list
     mybag.map(lambda d : pwstack_ensemble(d,control.data_mute,
                 control.stack_mute,
